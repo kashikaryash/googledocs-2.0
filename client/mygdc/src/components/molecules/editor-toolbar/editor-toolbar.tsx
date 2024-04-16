0a2881928cd144058cd2cc9eaf336c90d521cb5a
kashikaryash <kashikaryash@gmail.com>
2024-04-16 17:46:41 +0530

    com

--- client/mygdc/src/components/molecules/editor-toolbar/editor-toolbar.tsx
+++ client/mygdc/src/components/molecules/editor-toolbar/editor-toolbar.tsx
@@ -1,36 +1,113 @@
-window.global ||= window;
-import { useContext } from "react";
-import { EditorContext } from "../../../contexts/editor-context";
-import { EditorState } from "draft-js";
-import IconButton from "../../atoms/icon-button/icon-button";
-import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/outline";
-import FontSelect from "../../atoms/font-select/font-select";
+import React from "react";
+import FormatBoldIcon from "@material-ui/icons/FormatBold";
+import FormatItalicIcon from "@material-ui/icons/FormatItalic";
+import FormatUnderlinedIcon from "@material-ui/icons/FormatUnderlined";
+import StrikethroughSIcon from "@material-ui/icons/StrikethroughS";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+import ImageIcon from "@material-ui/icons/Image";
+import FormatAlignLeftIcon from "@material-ui/icons/FormatAlignLeft";
+import FormatAlignCenterIcon from "@material-ui/icons/FormatAlignCenter";
+import FormatAlignRightIcon from "@material-ui/icons/FormatAlignRight";
+import FormatAlignJustifyIcon from "@material-ui/icons/FormatAlignJustify";
+import UndoIcon from "@material-ui/icons/Undo";
+import RedoIcon from "@material-ui/icons/Redo";
+import CodeIcon from "@material-ui/icons/Code";
+import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
+import { IconButton, Select, MenuItem } from "@material-ui/core";
+// import { ChromePicker } from "react-color";
 
-const EditorToolbar = () => {
-  const { editorState, setEditorState } = useContext(EditorContext);
+interface EditorToolbarProps {
+  handleBoldClick: () => void;
+  handleItalicClick: () => void;
+  handleUnderlineClick: () => void;
+  handleStrikethroughClick: () => void;
+  handleBulletListClick: () => void;
+  handleNumberedListClick: () => void;
+  handleInsertImage: (imageUrl: string) => void;
+  handleTextColor: (color: string) => void;
+  handleTextBackgroundColor: (color: string) => void;
+  handleFontSizeChange: (newFontSize: number) => void;
+  handlePageAlignment: (alignment: "left" | "center" | "right" | "justify") => void;
+  handleUndoBtnClick: () => void;
+  handleRedoBtnClick: () => void;
+  handleInlineStyleToggle: (inlineStyle: string) => void;
+  handleBlockTypeToggle: (blockType: string) => void;
+}
 
-  const handleUndoBtnClick = () => {
-    setEditorState(EditorState.undo(editorState));
+const EditorToolbar: React.FC<EditorToolbarProps> = ({
+  handleBoldClick,
+  handleItalicClick,
+  handleUnderlineClick,
+  handleStrikethroughClick,
+  handleBulletListClick,
+  handleNumberedListClick,
+  handleInsertImage,
+  // handleTextColor,
+  // handleTextBackgroundColor,
+  handleFontSizeChange,
+  handlePageAlignment,
+  handleUndoBtnClick,
+  handleRedoBtnClick,
+  handleInlineStyleToggle,
+  handleBlockTypeToggle,
+}) => {
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const imageUrl = reader.result as string;
+        handleInsertImage(imageUrl);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
-  const handleRedoBtnClick = () => {
-    setEditorState(EditorState.redo(editorState));
+  // const handleTextColorChange = (color: string) => {
+  //   handleTextColor(color);
+  // };
+
+  // const handleTextBackgroundColorChange = (color: string) => {
+  //   handleTextBackgroundColor(color);
+  // };
+
+  const handleAlignmentChange = (alignment: "left" | "center" | "right" | "justify") => {
+    handlePageAlignment(alignment);
+  };
+
+  const handleFontSizeSelect = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const newFontSize = Number(event.target.value as string);
+    handleFontSizeChange(newFontSize);
   };
 
   return (
-    <div className="w-full h-9 px-3 py-1 flex-shrink-0 flex items-center">
-      <IconButton
-        onClick={handleUndoBtnClick}
-        icon={<ArrowLeftIcon className="h-4 w-4" />}
-        tooltip="Undo"
-      />
-      <IconButton
-        onClick={handleRedoBtnClick}
-        icon={<ArrowRightIcon className="h-4 w-4" />}
-        tooltip="Redo"
-      />
-      <div className="h-5 border-1 border-1-gray-300 mx-2"></div>
-      <FontSelect />
+    <div>
+      <IconButton onClick={handleBoldClick}><FormatBoldIcon /></IconButton>
+      <IconButton onClick={handleItalicClick}><FormatItalicIcon /></IconButton>
+      <IconButton onClick={handleUnderlineClick}><FormatUnderlinedIcon /></IconButton>
+      <IconButton onClick={handleStrikethroughClick}><StrikethroughSIcon /></IconButton>
+      <IconButton onClick={handleBulletListClick}><FormatListBulletedIcon /></IconButton>
+      <IconButton onClick={handleNumberedListClick}><FormatListNumberedIcon /></IconButton>
+      <input type="file" accept="image/*" onChange={handleImageUpload} style={{ display: "none" }} id="icon-button-file" />
+      <label htmlFor="icon-button-file">
+        <IconButton component="span"><ImageIcon /></IconButton>
+      </label>
+      <Select onChange={(e) => handleFontSizeSelect(e)} defaultValue="12">
+        <MenuItem value="12">12</MenuItem>
+        <MenuItem value="14">14</MenuItem>
+        <MenuItem value="16">16</MenuItem>
+        <MenuItem value="18">18</MenuItem>
+        <MenuItem value="20">20</MenuItem>
+      </Select>
+      <IconButton onClick={() => handleAlignmentChange("left")}><FormatAlignLeftIcon /></IconButton>
+      <IconButton onClick={() => handleAlignmentChange("center")}><FormatAlignCenterIcon /></IconButton>
+      <IconButton onClick={() => handleAlignmentChange("right")}><FormatAlignRightIcon /></IconButton>
+      <IconButton onClick={() => handleAlignmentChange("justify")}><FormatAlignJustifyIcon /></IconButton>
+      <IconButton onClick={handleUndoBtnClick}><UndoIcon /></IconButton>
+      <IconButton onClick={handleRedoBtnClick}><RedoIcon /></IconButton>
+      <IconButton onClick={() => handleInlineStyleToggle("code")}><CodeIcon /></IconButton>
+      <IconButton onClick={() => handleBlockTypeToggle("blockquote")}><FormatQuoteIcon /></IconButton>
     </div>
   );
 };
--- client/mygdc/src/components/organisms/document-editor/document-editor.tsx
+++ client/mygdc/src/components/organisms/document-editor/document-editor.tsx
@@ -1,16 +1,30 @@
 window.global ||= window;
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { EditorContext } from "../../../contexts/editor-context";
 import { Editor } from "draft-js";
+import "rabbitcss"
+import { ChatBubble, Send } from "@mui/icons-material";
 
 const DocumentEditor = () => {
   const { editorState, editorRef, handleEditorChange, focusEditor } =
     useContext(EditorContext);
 
+    const [bot  , setBot] = useState(false);
+
+    const setChat = () =>{ 
+        if(bot === true){
+          setBot(false)
+        }else{
+          setBot(true)
+        }
+    }
+
+
   return (
+    <div className="   flex ">
     <div
-      style={{ height: "1100px", width: "850px" }}
-      className="bg-white shadow-md flex-shrink-0 cursor-text p-12"
+      style={{ height: "1100px", width: "950px" }}
+      className=" bg-white shadow-2xl border border-black rounded-xl flex-shrink-0 cursor-text p-12 "
       onClick={focusEditor}
     >
       <Editor
@@ -18,6 +32,28 @@ const DocumentEditor = () => {
         editorState={editorState}
         onChange={handleEditorChange}
       />
+      
+    </div>
+    <div className="absolute bottom-6 right-5 rounded-full shadow-xl border border-black bg-grey-400 p-3" onClick={()=>setChat()}>
+        <ChatBubble className="text-blue-700"/>
+    </div>
+    {bot && 
+    
+    <div className=" p-5 flex justify-center flex-1  ">
+      <div className=" w-[80%] blue-blur-glass h-[500px] rounded-xl p-3 flex flex-col justify-center">
+        
+        <div className="bg-white text-black rounded-xl shadow-xl h-[80%] overflow-y-scroll text-xl font-mono p-3">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus et, quidem perspiciatis nihil dicta sequi excepturi, aut aperiam nesciunt qui aspernatur maiores nostrum ipsa unde culpa quae esse placeat doloremque obcaecati ad. Nemo.s
+        </div>
+        <div className="my-3 flex justify-center gap-3 items-center">
+        <input className="w-full p-3  rounded-full" placeholder="Enter Prompt Here" />
+        <Send className="text-blue-500"/>
+        </div>
+      </div>
+    </div>
+    
+    }
+    
     </div>
   );
 };
