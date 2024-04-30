41f7a07edda8d06ab6a0546c926a6e4df678e9e0
kashikaryash <kashikaryash@gmail.com>
2024-04-30 11:25:12 +0530

    commit

--- client/mygdc/src/components/organisms/document-editor/document-editor.tsx
+++ client/mygdc/src/components/organisms/document-editor/document-editor.tsx
@@ -1,19 +1,17 @@
 import { useState, useContext } from "react";
 import { EditorContext } from "../../../contexts/editor-context";
-import {FaMicrophone} from "react-icons/fa6"
-import { Editor, EditorState, RichUtils, Modifier, AtomicBlockUtils } from "draft-js";
-
+import { FaMicrophone } from "react-icons/fa6";
+import { Editor, EditorState, RichUtils, Modifier } from "draft-js";
+import 'regenerator-runtime/runtime';
 import "draft-js/dist/Draft.css";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 const DocumentEditor = () => {
-  const { editorState, setEditorState, editorRef,handleEditorChange, currentFont,focusEditor  } =
-    useContext(EditorContext);
-    
-    
-
+  const { editorState, setEditorState, editorRef, handleEditorChange } = useContext(EditorContext);
   const [loading, setLoading] = useState(false);
+  
 
-  const handleKeyCommand = (command: string, editorState: EditorState) => {
+  const handleKeyCommand = (command:string, editorState: EditorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
       setEditorState(newState);
@@ -22,24 +20,12 @@ const DocumentEditor = () => {
     return "not-handled";
   };
 
-  const toggleBlockType = (blockType: string) => {
-    setEditorState(RichUtils.toggleBlockType(editorState, blockType));
-  };
-
-  const toggleInlineStyle = (inlineStyle: string) => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
-  };
-
-  const handleChange = (editorState: EditorState) => {
-    setEditorState(editorState);
-  };
-
   const handleGenerateResponse = async () => {
     setLoading(true);
     const selectedText = editorState.getSelection().isCollapsed()
       ? null
       : editorState.getCurrentContent().getBlockForKey(editorState.getSelection().getStartKey()).getText();
-  
+
     if (selectedText) {
       try {
         const response = await fetch("http://localhost:5500/api/generate-response", {
@@ -49,17 +35,17 @@ const DocumentEditor = () => {
           },
           body: JSON.stringify({ message: selectedText })
         });
-  
+
         if (!response.ok) {
           throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
         }
-  
+
         const responseData = await response.json();
         const generatedText = responseData.response;
-  
+
         // Get the current selection
         const selection = editorState.getSelection();
-  
+
         // Create a new ContentState with the generated text inserted at the selection
         const contentState = editorState.getCurrentContent();
         const contentStateWithResponse = Modifier.replaceText(
@@ -67,65 +53,14 @@ const DocumentEditor = () => {
           selection,
           generatedText
         );
-  
+
         // Create a new EditorState with the updated ContentState
         const newEditorState = EditorState.push(
           editorState,
           contentStateWithResponse,
           'insert-characters'
         );
-  
-        // Set the new EditorState
-        setEditorState(newEditorState);
-      } catch (error) {
-        console.error("Error generating response:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  };
-  const handleGenerateText = async () => {
-    setLoading(true);
-    const selectedText = editorState.getSelection().isCollapsed()
-      ? null
-      : editorState.getCurrentContent().getBlockForKey(editorState.getSelection().getStartKey()).getText();
-  
-    if (selectedText) {
-      try {
-        const response = await fetch("http://localhost:5500/api/generate-response", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ message: selectedText })
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
-        }
-  
-        const responseData = await response.json();
-        const generatedText = responseData.response;
-  
-        // Get the current selection
-        const selection = editorState.getSelection();
-  
-        // Create a new ContentState with the generated text inserted at the selection
-        const contentState = editorState.getCurrentContent();
-        const contentStateWithResponse = Modifier.replaceText(
-          contentState,
-          selection,
-          generatedText
-        );
-        
-  
-        // Create a new EditorState with the updated ContentState
-        const newEditorState = EditorState.push(
-          editorState,
-          contentStateWithResponse,
-          'insert-characters'
-        );
-  
+
         // Set the new EditorState
         setEditorState(newEditorState);
       } catch (error) {
@@ -136,20 +71,17 @@ const DocumentEditor = () => {
     }
   };
 
-  const addPageBreak = () => {
-    const contentState = editorState.getCurrentContent();
-    const contentStateWithPageBreak = contentState.createEntity('PAGE_BREAK', 'IMMUTABLE', { style: { width: '100%', height: '1px', backgroundColor: 'black', display: 'block', marginBottom: '29cm' } });
-    const entityKey = contentStateWithPageBreak.getLastCreatedEntityKey();
-    const newEditorState = AtomicBlockUtils.insertAtomicBlock(editorState, entityKey, ' ');
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
 
-    setEditorState(newEditorState);
-  };
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
   return (
-    <div
-      style={{ height: "1100px", width: "850px" }}
-      className="bg-white text-black shadow-2xl flex-shrink-0 cursor-text p-12 ">
-      
+    <div style={{ height: "1100px", width: "850px" }} className="bg-white text-black shadow-2xl flex-shrink-0 cursor-text p-12 ">
+
       <Editor
         ref={editorRef}
         editorState={editorState}
@@ -158,27 +90,24 @@ const DocumentEditor = () => {
         spellCheck={true}
         placeholder="Type here..."
       />
-      
+{transcript}
       <div className="absolute top-[200px] right-[100px] w-[400px] flex flex-col gap-5 p-8 rounded-lg shadow-lg bg-white">
         <h2 className="text-4xl font-bold text-gray-900">Experience Generative AI</h2>
         <p className="text-gray-600">Select text and click the generate button to get an AI response.</p>
         <button onClick={handleGenerateResponse} disabled={loading} className="btn bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
           {loading ? 'Loading...' : 'Generate Response'}
         </button>
-        <button onClick={addPageBreak} className="btn bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-          Add Page Break
-        </button>
       </div>
 
       <div className="absolute top-[200px] left-[100px] w-[400px] flex flex-col gap-5 p-8 rounded-lg shadow-lg white-blur-glass items-center">
         <p className="text-gray-600">Click on Mic to speak.</p>
         <div className="p-3 border border-black rounded-full shadow-2xl blue-blur-glass">
-        < FaMicrophone onClick={handleGenerateText} className="w-[80px] h-[80px] text-white">
-          {loading ? 'Loading...' : 'Generate Response'}
-        </FaMicrophone>
+          <FaMicrophone onClick={startListening} className="w-[80px] h-[80px] text-white" />
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
         </div>
       </div>
-      
+
     </div>
   );
 };
